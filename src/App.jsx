76feac473a5b9fc2c499d1d7b37e2242afbc1150
limhyerin <rin091345@gmail.com>
2024-02-19 12:27:52 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Company from './pages/Company';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/company" element={<Company />}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
